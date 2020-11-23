e88063454d3c18c8152a0ff37cf1f29a4d5cf04c
Avraham Zilberman <az@oodidactics.com>
2020-11-23 11:35:57 +0200

    Add project files.

---
+++ UploadFileAPI/Controllers/UploadFileController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UploadFileAPI.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UploadFileAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadFileController : ControllerBase
+    {
+        // GET: api/<UploadFileController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UploadFileController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UploadFileController>
+        [HttpPost]
+        public void Post()
+        {
+            var request = HttpContext.Request;
+            var file = request.Form.Files[0];
+            //Path should be take froom up serttings.  
+            var path = @"C:\DOD\Dev\angular-course\drawingappclientdemo\src\assets\images\" + file.FileName;
+            using(var stream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+        }
+
+        // PUT api/<UploadFileController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UploadFileController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ UploadFileAPI/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadFileAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.Configure<FormOptions>(x =>
+            {
+                x.ValueLengthLimit = 5000; // Limit on individual form values
+                x.MultipartBodyLengthLimit = 104857600; // Limit on form body size
+                x.MultipartHeadersLengthLimit = 737280000; // Limit on form header size
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+           
+        }
+    }
+}
